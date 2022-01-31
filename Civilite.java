f545e2e5b56ddc2d983384b6502c7cec8fd5b83b
[WCS/SXB] Joshua Arus <joshua.arus@wildcodeschool.com>
2022-01-31 16:29:12 +0100

    Ajout correction

---
+++ Civilite.java
@@ -0,0 +1,5 @@
+public enum Civilite {
+    Mr,
+    Mme,
+    Autre
+}
---
+++ Utilisateur.java
@@ -0,0 +1,13 @@
+public class Utilisateur {
+
+    public String Nom;
+    public String Prenom;
+    public Civilite Civilite;
+
+    public Utilisateur(String nom, String prenom, Civilite civilite) {
+        Nom = nom;
+        Prenom = prenom;
+        Civilite = civilite;
+    }
+    
+}
